01ccf64d653bf69a6a2a25e690594263b6b6b1e3
Kalu <info@zap.de.com>
2016-05-17 15:26:11 +0200

    loadout / eft refactor

--- packages/desc-loadoutdisplay/lib/templates/loadout.js
+++ packages/desc-loadoutdisplay/lib/templates/loadout.js
@@ -1,4 +1,19 @@
 Meteor.startup(function() {
+	function SelectText(text) {
+	    doc = document;
+	    if(doc.body.createTextRange) {
+	        range = document.body.createTextRange();
+	        range.moveToElementText(text);
+	        range.select();
+	    } else if (window.getSelection) {
+	        selection = window.getSelection();      
+	        range = document.createRange();
+	        range.selectNodeContents(text);
+	        selection.removeAllRanges();
+	        selection.addRange(range);
+	    }
+      }
+
 	Template['rackSmall'].helpers({
 		filled: function(modules) {
 			return (modules.length > 0);
