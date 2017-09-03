258d73d5a71a05b2d77b75e74e0d239b6ce45a34
lunedis <info@zap.de.com>
2017-09-03 22:06:13 +0200

    bash scripts

---
+++ export.sh
@@ -0,0 +1,2 @@
+mongoexport --host localhost --port 27017 --db fourierv2 --collection doctrines --jsonArray -o doctrines.json
+mongoexport --host localhost --port 27017 --db fourierv2 --collection fittings --jsonArray -o fittings.json
---
+++ invTypesUpdate.sh
@@ -0,0 +1,1 @@
+mongoimport --host localhost --port 27017 --db fourierv2 --collection invtypes --drop --jsonArray --file invTypes.json
